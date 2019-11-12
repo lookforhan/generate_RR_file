803c97426b83cd6807f805dfa34b35eac4a45ab0
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-11-12 20:14:45 +0800

    Update application.m
    增加不同烈度

--- application/application.m
+++ application/application.m
@@ -4,11 +4,14 @@
 t = GRF_EMT('MOD.inp');
 t.collect_basic_information;
 num = numel(t.Pipe_ID);
-t.Repair_Rate = num2cell(0.1254*ones(num,1));
+pgv_IX = 0.1254;
+pgv_VIII = 0.0542;
+pgv_VII = 0.0246;
+t.Repair_Rate = num2cell(pgv_VIII*ones(num,1));
 t.Material = repmat({'CI'},[num,1]);
 t.check_data;
 t.generate2table;
 t.RR_table
-writetable(t.RR_table,'mod_rr.txt',...
+writetable(t.RR_table,'MOD_rr_viii.txt',...
     'Delimiter','\t')
 t.delete
